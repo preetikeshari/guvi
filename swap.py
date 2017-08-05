6d625b2abd85e87c27a29bf32de91ec67af90761
preetikeshari <30740806+preetikeshari@users.noreply.github.com>
2017-08-05 11:01:09 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,7 @@
+my_string="i am preeti keshari"
+x=input("please enter a string")
+reversed_str = " ".join(my_string.split(" ")[::-1])
+reversed_str1 = " ".join(my_string.split(" ")[::-1])
+print(reversed_str)
+print(reversed_str1)
+ 
